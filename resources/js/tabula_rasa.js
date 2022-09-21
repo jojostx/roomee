27828aa17bb3cb3772bea6dc9ee7577101550232
Ari Jay <ikuskid7@gmail.com>
2022-09-21 06:11:34 +0100

    refactor colors to use specified alias from tailwind config

--- resources/js/tabula_rasa.js
+++ resources/js/tabula_rasa.js
@@ -1,19 +1,21 @@
 require('./bootstrap');
 
 import Cropper from "cropperjs";
-import Alpine from 'alpinejs'
-import focus from '@alpinejs/focus'
-import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
 import multiselect from './alpinejs/filamentphp/multi-select'
 import customPhotoUploadFormComponent from './alpinejs/filamentphp/photo-upload'
+import focus from '@alpinejs/focus'
 
-window.Alpine = Alpine;
+import Alpine from 'alpinejs'
+import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
+import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
+
+window.Alpine = Alpine
 window.Cropper = Cropper;
 
 Alpine.data('multiselect', multiselect);
-
-Alpine.plugin(FormsAlpinePlugin)
 Alpine.plugin(customPhotoUploadFormComponent)
 Alpine.plugin(focus)
+Alpine.plugin(FormsAlpinePlugin)
+Alpine.plugin(NotificationsAlpinePlugin)
 
 Alpine.start();
