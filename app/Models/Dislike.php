008b74ec689a3a2b8e8121621de99adedb9308b6
Ari Jay <ikuskid7@gmail.com>
2022-09-24 16:17:10 +0100

    add ide-helper mixins to models

--- app/Models/Dislike.php
+++ app/Models/Dislike.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperDislike
+ */
 class Dislike extends Model
 {
     use HasFactory;
--- app/Models/Town.php
+++ app/Models/Town.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin IdeHelperTown
+ */
 class Town extends Model
 {
     use HasFactory;
