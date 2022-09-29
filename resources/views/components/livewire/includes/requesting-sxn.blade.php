f670768c2ba0c56ef1aab8402724e13fdf02e4fc
Ari Jay <ikuskid7@gmail.com>
2022-09-29 16:01:00 +0100

    UI changes

--- resources/views/components/livewire/includes/requesting-sxn.blade.php
+++ resources/views/components/livewire/includes/requesting-sxn.blade.php
@@ -1,37 +1,23 @@
 @props(['user' => null])
 
-<div>
+<div class="flex items-center w-full gap-2">
     <!-- requesting and contacting section -->
     @if (auth()->user()->isRoommateWith($user))
-    <x-button-primary class="ml-auto">
-        <x-slot name="svgPath">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </x-slot>
+    <x-filament-support::button labelSrOnly="true" tag="a" size="sm" icon="heroicon-s-phone-outgoing" aria-label="contact {{ $user->fullname }}" title="contact {{ $user->fullname }}">
         Contact
-    </x-button-primary>
+    </x-filament-support::button>
     @elseif (auth()->user()->hasSentRoommateRequestTo($user))
-    <x-button-sec wire:click="showDeleteRequestPopup()" aria-label="delete request" title="delete request">
-        <x-slot name="svgPath">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M13 7a4 4 0 11-8 0 4 4 0 018 0zM9 14a6 6 0 00-6 6v1h12v-1a6 6 0 00-6-6zM21 12h-6" />
-        </x-slot>
+    <x-filament-support::button class="inline-flex items-center justify-center w-full px-5" wire:click="showDeleteRequestPopup()" size="sm" icon="heroicon-s-user-remove" aria-label="delete request" title="delete request">
         Requested
-    </x-button-sec>
+    </x-filament-support::button>
     @elseif (auth()->user()->hasRoommateRequestFrom($user))
-    <a href="{{ route('requests') }}#requests-recieved__{{ $user->id }}" aria-label="view request" title="view request" class="inline-flex items-center px-1 py-1 text-xs font-semibold transition duration-150 ease-in-out rounded-md text-primary-600 bg-primary-100 xs:px-2 sm:text-sm hover:text-primary-700 hover:bg-primary-200 focus:outline-none focus:bg-primary-200 focus:text-primary-700">
-        <span class="pr-1">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" class="w-3 h-3 sm:w-4 sm:h-4" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />               
-            </svg>
-        </span>
+    <x-filament-support::button class="inline-flex items-center justify-center w-full px-5" href="{{ route('requests') }}#requests-recieved__{{ $user->id }}" tag="a" :outlined="true" size="sm" icon="heroicon-s-external-link" aria-label="view request" title="view request">
         View request
-    </a>
+    </x-filament-support::button>
     @else
-    <x-button-primary wire:click="sendRequest()" aria-label="send request" title="send request">
-        <x-slot name="svgPath">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-        </x-slot>
+    <x-filament-support::button class="inline-flex items-center justify-center w-full px-5" wire:click="sendRequest()" :outlined="true" color="secondary" size="sm" icon="heroicon-s-user-add" aria-label="send request" title="send request">
         Request
-    </x-button-primary>
+    </x-filament-support::button>
     @endif
     <!-- end of requesting and contacting section -->
 </div>
--- resources/views/livewire/components/cards/dashboard-card.blade.php
+++ resources/views/livewire/components/cards/dashboard-card.blade.php
@@ -1,14 +1,14 @@
 <div class="w-full col-span-1 bg-white border divide-y rounded-md shadow">
     <div class="flex items-center justify-between px-4 py-2">
         <div class="flex items-center">
-            <div class="mr-2 overflow-hidden border-2 rounded-full border-primary-500 lg:mr-3 w-9 h-9">
-                <img id="avatar" src="{{ $user->avatar_path ?? asset('images/avatar_placeholder.png') }}" alt="{{ $user->firstname }}'s avatar" width="100%" height="100%">
+            <div class="w-10 h-10 mr-2 overflow-hidden border-2 rounded-full border-primary-500 lg:mr-3">
+                <img id="avatar" src="{{ $user->avatar_path ?? asset('images/avatar_placeholder.png') }}" alt="{{ $user->firstname }}'s avatar" class="w-full">
             </div>
-            <div class="max-w-[140px] overflow-ellipsis overflow-x-clip">
-                <p>
+            <div class="max-w-[160px]">
+                <p class="overflow-x-hidden text-base font-semibold text-gray-700 text-ellipsis">
                     {{ $user->fullname }}
                 </p>
-                <p class="text-sm leading-none text-secondary-600">
+                <p title="{{ $user->email }}" class="overflow-x-hidden text-sm text-ellipsis text-secondary-500">
                     {{ $user->email }}
                 </p>
             </div>
@@ -23,8 +23,16 @@
     </div>
 
     <div class="flex flex-row-reverse items-center gap-2 px-4 py-2">
-        <x-livewire.includes.requesting-sxn :user="$user" />
-        <x-livewire.includes.favoriting-sxn :user="$user" />
         <span class="mr-auto text-xs font-semibold text-danger-700">{{ $user->similarity_score }}%</span>
+    </div>
+
+    <div class="flex items-center gap-2 px-4 py-2">
+        <x-livewire.includes.favoriting-sxn :user="$user" />
+        <x-livewire.includes.requesting-sxn :user="$user" />
+        <button class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-300 rounded-lg shrink-0 focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
+            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+            </svg>
+        </button>
     </div>
 </div>
