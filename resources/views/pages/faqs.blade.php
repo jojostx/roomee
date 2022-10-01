0404bded5381bb6e2037525eac989e08a0fc0245
Ari Jay <ikuskid7@gmail.com>
2022-10-01 17:16:42 +0100

    padding and layout fix

--- resources/views/pages/faqs.blade.php
+++ resources/views/pages/faqs.blade.php
@@ -1,13 +1,11 @@
 <x-landing-layout>
     <x-slot name="banner">
-        <div class="flex items-center justify-center text-center bg-primary-100 h-60 md:h-80">
-            <p class="mt-10 mb-6 text-3xl font-extrabold text-secondary-700 md:text-4xl lg:text-5xl">Frequently Asked Questions</p>
+        <div class="flex items-center justify-center px-4 py-24 text-center bg-primary-100 h-60 md:h-80">
+            <p class="text-3xl font-extrabold text-secondary-700 md:text-4xl lg:text-5xl">Frequently Asked Questions</p>
         </div>
     </x-slot>
 
-    <div class="absolute top-0 h-screen hero">
-
-    </div>
+    <div class="absolute top-0 h-screen hero"></div>
 
     <!-- FAQs Accordion -->
     <div class="px-4">
@@ -67,7 +65,7 @@
     <!-- End Of Accordion -->
 
     <!-- Feedback Section -->
-    <div class="py-16 bg-secondary-100">
+    <div class="px-4 py-16 bg-secondary-100">
         <div class="flex flex-col items-center w-10/12 mx-auto">
             <p class="mb-4 text-xl font-semibold">Was this article helpful?</p>
             <form x-on:submit.prevent="submitForm('http://127.0.0.1:8000/faqs')" x-data="form('feedback_Form')" method="POST" id="feedback_Form" class="flex flex-col items-center justify-center mb-4">
@@ -90,12 +88,12 @@
                     </label>
                 </div>
 
-                <x-button type="Submit" class="flex items-center justify-center font-bold text-secondary-100 bg-secondary-800 rounded-full w-28 hover:text-secondary-100 hover:bg-primary-500">
+                <x-button type="Submit" class="flex items-center justify-center font-bold rounded-full text-secondary-100 bg-secondary-800 w-28 hover:text-secondary-100 hover:bg-primary-500">
                     Submit
                 </x-button>
             </form>
 
-            <div id="flash" class="hidden px-4 py-2 font-bold text-primary-600 bg-white border rounded-md shadow-md">
+            <div id="flash" class="hidden px-4 py-2 font-bold bg-white border rounded-md shadow-md text-primary-600">
             </div>
 
         </div>
@@ -103,16 +101,16 @@
     <!-- End of Feedback Section -->
 
     <!-- call to action //links to contact page -->
-    <div class="flex justify-center bg-primary-300">
+    <div class="flex justify-center px-4 bg-primary-300">
         <div class="flex flex-col items-center justify-between w-11/12 my-12 py-14 md:flex-row">
             <div class="max-w-lg p-8 border-2 border-secondary-800 sm:p-12 md:mb-0">
                 <p class="text-2xl font-bold md:text-3xl">Feel free to contact us if this article did not answer all your questions.</p>
             </div>
 
-            <div class="w-1 h-12 border-l-2 border-secondary-900 border-dashed md:border-t-2 md:w-full md:h-1"></div>
+            <div class="w-1 h-12 border-l-2 border-dashed border-secondary-900 md:border-t-2 md:w-full md:h-1"></div>
 
-            <!-- <button class="bg-secondary-800 rounded-full"><a class="inline-block px-6 py-4 text-2xl font-bold text-white" href="{{ route('contact') }}">Get in touch</a></button> -->
-            <button class="flex-shrink-0 bg-secondary-800 rounded-full hover:bg-primary-500 hover:shadow-md"><a class="inline-block px-6 py-4 text-2xl font-bold text-white" href="{{ route('contact') }}">Get in touch</a></button>
+            <!-- <button class="rounded-full bg-secondary-800"><a class="inline-block px-6 py-4 text-2xl font-bold text-white" href="{{ route('contact') }}">Get in touch</a></button> -->
+            <button class="flex-shrink-0 rounded-full bg-secondary-800 hover:bg-primary-500 hover:shadow-md"><a class="inline-block px-6 py-4 text-2xl font-bold text-white" href="{{ route('contact') }}">Get in touch</a></button>
         </div>
     </div>
     <!-- end of call to action -->
