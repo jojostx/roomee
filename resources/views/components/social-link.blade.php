74b88fb5a75f5b13c6e5df014baa1f0ebf55a8f0
Ari Jay <ikuskid7@gmail.com>
2022-10-01 16:57:38 +0100

    hover color fix

--- resources/views/components/social-link.blade.php
+++ resources/views/components/social-link.blade.php
@@ -1,3 +1,3 @@
-<svg viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => 'mr-6 w-6 text-secondary-400 focus:text-secondary-200 focus:outline-none hover:text-secondary-200']) }}>
+<svg viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {{ $attributes->merge(['class' => 'cursor-pointer mr-6 w-6 text-secondary-400 focus:text-secondary-200 focus:outline-none']) }}>
     {{ $slot }}
 </svg>
