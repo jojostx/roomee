ed8c390936e9bc7562fbd634b59846a677d21473
Ari Jay <ikuskid7@gmail.com>
2022-10-11 15:10:30 +0100

    refactor: use trait that satisfies abstract methods contract; extract actions to reusable component methods; register event listeners and emit event (refresh:component) after action in order to refresh component UI;

--- app/Http/Livewire/Pages/DashboardFilament.php
+++ app/Http/Livewire/Pages/DashboardFilament.php
@@ -2,27 +2,39 @@
 
 namespace App\Http\Livewire\Pages;
 
-use App\Http\Livewire\Traits\WithFavoriting;
-use App\Http\Livewire\Traits\WithRequesting;
 use Closure;
-use App\Models\RoommateRequest;
+use Filament\Forms;
 use App\Models\User;
-use Livewire\Component;
 use Filament\Tables;
-use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Database\Eloquent\Builder;
+use App\Models\Report;
+use Livewire\Component;
+use App\Http\Livewire\Traits;
+use App\Models\RoommateRequest;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\Pagination\Paginator;
 
 class DashboardFilament extends Component implements Tables\Contracts\HasTable
 {
-    use Tables\Concerns\InteractsWithTable {
+    use
+        Traits\WithFavoriting,
+        Traits\WithRequesting,
+        Traits\WithBlocking,
+        Traits\CanRetrieveUser,
+        Tables\Concerns\InteractsWithTable {
         applySortingToTableQuery as parentApplySortingToTableQuery;
     }
-    use WithFavoriting, WithRequesting;
 
     public Collection $similarity_scores;
+
+    protected function getListeners()
+    {
+        return [
+            'refresh:component' => '$refresh',
+        ];
+    }
 
     public function getAuthModel(): ?User
     {
@@ -112,108 +124,20 @@ class DashboardFilament extends Component implements Tables\Contracts\HasTable
         ];
     }
 
-    // protected function getTableActions(): array
-    // {
-    //     return [
-    //         Tables\Actions\Action::make('request')
-    //             ->button()
-    //             ->outlined(fn (User $record): bool => $this->hasNotSentOrRecievedRoommateRequest($record))
-    //             ->label(function (User $record): string {
-    //                 return match (true) {
-    //                     $this->hasNotSentOrRecievedRoommateRequest($record) => 'Send Request',
-    //                     $this->hasRoommateRequestFrom($record) => 'Accept Request',
-    //                     $this->hasSentRoommateRequestTo($record) => 'Delete Request',
-    //                     default => 'Contact ' . ucfirst($record->full_name)
-    //                 };
-    //             })
-    //             ->icon(function (User $record): string {
-    //                 if ($this->hasNotSentOrRecievedRoommateRequest($record)) {
-    //                     return 'heroicon-s-user-add'; //+
-    //                 } elseif ($this->hasRoommateRequestFrom($record)) { //@duplicate query #2
-    //                     return 'heroicon-s-check-circle'; //V
-    //                 } elseif ($this->hasSentRoommateRequestTo($record)) { //@duplicate query #3
-    //                     return 'heroicon-s-user-remove'; //-
-    //                 } else {
-    //                     return 'heroicon-s-phone-outgoing'; //c
-    //                 }
-    //             })
-    //             ->color(fn (User $record): string => $this->hasNotSentOrRecievedRoommateRequest($record) ? 'secondary' : 'primary')
-    //             ->extraAttributes([
-    //                 'title' => 'send request',
-    //                 'class' => 'w-full',
-    //                 'id' => 'filament-tables-action-request'
-    //             ]),
+    protected function getTableActions(): array
+    {
+        return [
+            ...$this->getRequestingActions(),
+            ...$this->getFavoritingActions(),
 
-    //         Tables\Actions\Action::make('favorite')
-    //             ->iconButton()
-    //             ->label('Favorite')
-    //             ->tooltip('Add to Favorites')
-    //             ->color('secondary')
-    //             ->icon('heroicon-o-star')
-    //             ->action(function (User $record) {
-    //                 $this->favorite($record);
-    //             })
-    //             ->extraAttributes([
-    //                 'title' => 'Favorite button',
-    //                 'class' => 'border border-secondary-300',
-    //                 'style' => 'width: 3rem; border-radius: .5rem',
-    //                 'id' => 'filament-tables-action-favorite'
-    //             ])
-    //             ->visible(fn (User $record): bool => !$this->hasBeenFavorited($record)),
-    //         Tables\Actions\Action::make('unfavorite')
-    //             ->iconButton()
-    //             ->label('Unfavorite')
-    //             ->tooltip('Remove from Favorites')
-    //             ->color('primary')
-    //             ->icon('heroicon-s-star')
-    //             ->action(function (User $record) {
-    //                 $this->unfavorite($record);
-    //             })
-    //             ->extraAttributes([
-    //                 'title' => 'Favorite button',
-    //                 'class' => 'border border-secondary-300',
-    //                 'style' => 'width: 3rem; border-radius: .5rem',
-    //                 'id' => 'filament-tables-action-favorite'
-    //             ])
-    //             ->visible(fn (User $record): bool => $this->hasBeenFavorited($record)),
-
-    //         Tables\Actions\ActionGroup::make([
-    //             Tables\Actions\Action::make('report')
-    //                 ->icon('heroicon-o-flag')
-    //                 ->color('warning')
-    //                 ->requiresConfirmation()
-    //                 ->modalHeading(fn (User $record) => 'Report ' . $record->full_name)
-    //                 ->modalSubheading('Select the relevant issues to submit a Report.')
-    //                 ->modalButton('Submit')
-    //                 ->modalWidth('sm')
-    //                 ->form([
-    //                     Forms\Components\CheckboxList::make('report_ids')
-    //                         ->label('Reports')
-    //                         ->options(Report::pluck('description', 'id')->transform(fn ($val) => ucfirst($val)))
-    //                         ->required()
-    //                         ->exists('reports', 'id')
-    //                         ->extraAttributes(['class' => 'space-y-2']),
-    //                 ])
-    //                 ->action(function (User $record, array $data) {
-    //                     if ($this->getAuthModel()->reportUser($record, $data['report_ids'])) {
-    //                         Notification::make()
-    //                             ->title("Report submitted succesfully")
-    //                             ->body("Your report has been submitted. Our team will review your report ASAP. Thanks!")
-    //                             ->success()
-    //                             ->seconds(15)
-    //                             ->send();
-    //                     }
-    //                 }),
-
-    //             Tables\Actions\Action::make('block')
-    //                 ->icon('heroicon-o-ban')
-    //                 ->color('danger')
-    //                 ->extraAttributes(['class' => 'mt-1']),
-    //         ])
-    //             ->color('gray')
-    //             ->icon('heroicon-o-dots-vertical'),
-    //     ];
-    // }
+            Tables\Actions\ActionGroup::make([
+                ...$this->getReportingAction(),
+                ...$this->getBlockingActions(),
+            ])
+                ->color('gray')
+                ->icon('heroicon-o-dots-vertical'),
+        ];
+    }
 
     protected function getTableRecordsPerPage(): int
     {
@@ -249,18 +173,27 @@ class DashboardFilament extends Component implements Tables\Contracts\HasTable
         return $this->getAuthModel()->getRoommateRequests();
     }
 
-    /** checks */
-    public function hasBeenFavorited(User $user): bool
+    public function getFavoritesProperty(): Collection
     {
-        return DB::table('favorites')
-            ->where([
-                'favoriter_id' => $this->getAuthModel()->id,
-                'favoritee_id' => $user->id
-            ])
-            ->exists();
+        return $this->getAuthModel()->favorites()->get(['favoritee_id']);
     }
 
-    public function hasRoommateRequestFrom(User $user): bool
+    /** checks */
+    protected function hasBeenBlocked(User $user): bool
+    {
+        return $this->blockedUsers
+            ->pluck('blockee_id_id')
+            ->contains($user->id);
+    }
+
+    protected function hasBeenFavorited(User $user): bool
+    {
+        return $this->favorites
+            ->pluck('favoritee_id')
+            ->contains($user->id);
+    }
+
+    protected function hasRoommateRequestFrom(User $user): bool
     {
         return $this->roommateRequests
             ->contains(function (RoommateRequest $roommateRequest) use ($user) {
@@ -268,7 +201,7 @@ class DashboardFilament extends Component implements Tables\Contracts\HasTable
             });
     }
 
-    public function hasSentRoommateRequestTo(User $user): bool
+    protected function hasSentRoommateRequestTo(User $user): bool
     {
         return $this->roommateRequests
             ->contains(function (RoommateRequest $roommateRequest) use ($user) {
@@ -276,7 +209,7 @@ class DashboardFilament extends Component implements Tables\Contracts\HasTable
             });
     }
 
-    public function hasSentOrRecievedRoommateRequest(User $user): bool
+    protected function hasSentOrRecievedRoommateRequest(User $user): bool
     {
         return $this->roommateRequests
             ->contains(function (RoommateRequest $roommateRequest) use ($user) {
@@ -284,9 +217,189 @@ class DashboardFilament extends Component implements Tables\Contracts\HasTable
             });
     }
 
-    public function hasNotSentOrRecievedRoommateRequest(User $user): bool
+    protected function hasNotSentOrRecievedRoommateRequest(User $user): bool
     {
         return !$this->hasSentOrRecievedRoommateRequest($user);
+    }
+
+    // actions
+    protected function getReportingAction()
+    {
+        return [
+            Tables\Actions\Action::make('report')
+            ->label('Report User')
+            ->icon('heroicon-o-flag')
+            ->color('warning')
+            ->requiresConfirmation()
+            ->modalHeading(fn (User $record) => 'Report ' . $record->full_name)
+            ->modalSubheading('Select the relevant Issues to submit a Report.')
+            ->modalButton('Submit')
+            ->modalWidth('sm')
+            ->form([
+                Forms\Components\CheckboxList::make('report_ids')
+                    ->label('Reports')
+                    ->options(Report::pluck('description', 'id')->transform(fn ($val) => ucfirst($val)))
+                    ->required()
+                    ->exists('reports', 'id')
+                    ->extraAttributes(['class' => 'space-y-2']),
+            ])
+            ->action(function (User $record, array $data) {
+                if ($this->getAuthModel()->reportUser($record, $data['report_ids'])) {
+                    Notification::make()
+                        ->title("Report submitted succesfully")
+                        ->body("Your report has been submitted. Our team will review your report ASAP. Thanks!")
+                        ->success()
+                        ->seconds(15)
+                        ->send();
+                }
+            }),
+        ];
+    }
+
+    protected function getBlockingActions()
+    {
+        return [
+            Tables\Actions\Action::make('block')
+                ->label('Block User')
+                ->icon('heroicon-o-lock-closed')
+                ->color('danger')
+                ->action(function (User $record) {
+                    $this->blockUser($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->requiresConfirmation()
+                ->modalHeading(fn (User $record) => 'Block ' . $record->full_name)
+                ->modalContent(fn (User $record) => str("<p class='text-center'>This will prevent <span class='font-semibold text-secondary-600'>{$record->full_name}</span> from viewing your profile and sending you Roommate requests.</p>")->toHtmlString())
+                ->extraAttributes(['class' => 'mt-1'])
+                ->visible(fn (User $record): bool => !$this->hasBeenBlocked($record)),
+
+            Tables\Actions\Action::make('unblock')
+                ->label('Unblock User')
+                ->icon('heroicon-o-lock-open')
+                ->color('danger')
+                ->action(function (User $record) {
+                    $this->unblockUser($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->requiresConfirmation()
+                ->modalHeading(fn (User $record) => 'Unblock ' . $record->full_name)
+                ->modalContent(fn (User $record) => str("<p class='text-center'>This will allow <span class='font-semibold text-secondary-600'>{$record->full_name}</span> to view your profile and send you Roommate requests.</p>")->toHtmlString())
+                ->extraAttributes(['class' => 'mt-1'])
+                ->visible(fn (User $record): bool => $this->hasBeenBlocked($record))
+        ];
+    }
+
+    protected function getFavoritingActions()
+    {
+        return [
+            Tables\Actions\Action::make('favorite')
+                ->iconButton()
+                ->label('Add to Favorites')
+                ->tooltip('Add to Favorites')
+                ->color('secondary')
+                ->icon('heroicon-o-star')
+                ->action(function (User $record) {
+                    $this->favorite($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->extraAttributes([
+                    'title' => 'Favorite button',
+                    'class' => 'border border-secondary-300',
+                    'style' => 'width: 3rem; border-radius: .5rem',
+                    'id' => 'filament-tables-action-favorite'
+                ])
+                ->visible(fn (User $record): bool => !$this->hasBeenFavorited($record)),
+
+            Tables\Actions\Action::make('unfavorite')
+                ->iconButton()
+                ->label('Remove from Favorites')
+                ->tooltip('Remove from Favorites')
+                ->color('primary')
+                ->icon('heroicon-s-star')
+                ->action(function (User $record) {
+                    $this->unfavorite($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->extraAttributes([
+                    'title' => 'Favorite button',
+                    'class' => 'border border-secondary-300',
+                    'style' => 'width: 3rem; border-radius: .5rem',
+                    'id' => 'filament-tables-action-favorite'
+                ])
+                ->visible(fn (User $record): bool => $this->hasBeenFavorited($record))
+        ];
+    }
+
+    protected function getRequestingActions()
+    {
+        return [
+            Tables\Actions\Action::make('send-request')
+                ->button()
+                ->outlined()
+                ->label('Send Request')
+                ->icon('heroicon-s-user-add')
+                ->color('secondary')
+                ->extraAttributes([
+                    'title' => 'send request',
+                    'class' => 'w-full filament-tables-action-send-request',
+                ])
+                ->action(function (User $record) {
+                    $this->sendRequest($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->requiresConfirmation()
+                ->modalHeading('Send Roommate Request')
+                ->modalContent(fn (User $record) => str("<p class='text-center'>This will send a Roommate request to <span class='font-semibold text-secondary-600'>{$record->full_name}</span>.</p>")->toHtmlString())
+                ->visible(fn (User $record) => $this->hasNotSentOrRecievedRoommateRequest($record)),
+
+            Tables\Actions\Action::make('accept-request')
+                ->button()
+                ->label('Accept Request')
+                ->icon('heroicon-s-check-circle')
+                ->color('primary')
+                ->extraAttributes([
+                    'title' => 'send request',
+                    'class' => 'w-full filament-tables-action-accept-request',
+                ])
+                ->action(function (User $record) {
+                    $this->acceptRequest($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->requiresConfirmation()
+                ->modalHeading('Accept Roommate Request')
+                ->modalContent(fn (User $record) => str("<p class='text-center'>This will enable <span class='font-semibold text-secondary-600'>{$record->full_name}</span> to contact you via your configured Contact channels.</p>")->toHtmlString())
+                ->visible(fn (User $record) => $this->hasRoommateRequestFrom($record)),
+
+            Tables\Actions\Action::make('delete-request')
+                ->button()
+                ->label('Delete Request')
+                ->icon('heroicon-s-user-remove')
+                ->color('danger')
+                ->extraAttributes([
+                    'title' => 'send request',
+                    'class' => 'w-full filament-tables-action-delete-request',
+                ])
+                ->action(function (User $record) {
+                    $this->deleteRequest($record);
+                    $this->emitSelf('refresh:component');
+                })
+                ->requiresConfirmation()
+                ->modalHeading('Delete Roommate Request')
+                ->modalContent(fn (User $record) => str("<p class='text-center'>This will delete the Roommate request you sent to <span class='font-semibold text-secondary-600'>{$record->full_name}</span>.</p>")->toHtmlString())
+                ->visible(fn (User $record) => $this->hasSentRoommateRequestTo($record)),
+
+            Tables\Actions\Action::make('contact-user')
+                ->button()
+                ->label(fn (User $record): string => 'Contact ' . ucfirst($record->full_name))
+                ->icon('heroicon-s-phone-outgoing')
+                ->color('success')
+                ->extraAttributes([
+                    'title' => 'send request',
+                    'class' => 'w-full filament-tables-action-contact-user',
+                ])
+                ->requiresConfirmation()
+                ->visible(fn (User $record) => $this->getAuthModel()->isRoommateWith($record)),
+        ];
     }
 
     public function render()
