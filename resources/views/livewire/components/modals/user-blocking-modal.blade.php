61612b070d63d03b24d0f49deb061083fe059613
Ari Jay <ikuskid7@gmail.com>
2022-10-12 12:38:50 +0100

    refactor: use direct access chacks in place of gates and policy for UI definitions

--- resources/views/livewire/components/modals/user-blocking-modal.blade.php
+++ resources/views/livewire/components/modals/user-blocking-modal.blade.php
@@ -1,6 +1,22 @@
 <div>
+    @if (auth()->user()->hasBlocked($user))
+    <!-- unblock user modal -->
+    <div class="flex items-center justify-center py-3 border-b border-secondary-300">
+        <p class="font-semibold text-secondary-700">
+            Unblock <span class="user-name">{{ $user->full_name }}</span>?
+        </p>
+    </div>
+    <div class="px-4 py-2">
+        <p class="text-secondary-600">
+            <span class="font-medium text-secondary-900">{{ $user->full_name }}</span> will be able to view your profile or send you roommate request.
+        </p>
+        <div class="flex justify-end pt-2">
+            <button wire:click="$emit('closeModal')" class="px-1 py-2 mr-4 font-semibold hover:text-primary-500 focus:text-primary-600 focus:outline-none">Cancel</button>
+            <button wire:click="submit" class="px-1 py-2 font-semibold text-danger-500 hover:text-danger-700 focus:text-danger-700 focus:outline-none">Unblock</button>
+        </div>
+    </div>
+    @else
     <!-- block user modal -->
-    @can('block', $user)
     <div class="flex items-center justify-center py-3 border-b border-secondary-300">
         <p class="font-semibold text-secondary-700">
             Block <span class="user-name">{{ $user->full_name }}</span>?
@@ -15,23 +31,5 @@
             <button wire:click="submit" class="px-1 py-2 font-semibold text-danger-500 hover:text-danger-700 focus:text-danger-700 focus:outline-none">Block</button>
         </div>
     </div>
-    @endcan
-
-    <!-- unblock user modal -->
-    @can('unblock', $user)
-    <div class="flex items-center justify-center py-3 border-b border-secondary-300">
-        <p class="font-semibold text-secondary-700">
-            Unblock <span class="user-name">{{ $user->full_name }}</span>?
-        </p>
-    </div>
-    <div class="px-4 py-2">
-        <p class="text-secondary-600">
-            <span class="font-medium text-secondary-900">{{ $user->full_name }}</span> will be able to view your profile or send you a roommate request.
-        </p>
-        <div class="flex justify-end pt-2">
-            <button wire:click="$emit('closeModal')" class="px-1 py-2 mr-4 font-semibold hover:text-primary-500 focus:text-primary-600 focus:outline-none">Cancel</button>
-            <button wire:click="submit" class="px-1 py-2 font-semibold text-danger-500 hover:text-danger-700 focus:text-danger-700 focus:outline-none">Unblock</button>
-        </div>
-    </div>
-    @endcan
+    @endif
 </div>
