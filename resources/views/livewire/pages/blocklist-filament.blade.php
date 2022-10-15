712a4c0594075070979ae44ae4b164a707c4f56a
Ari Jay <ikuskid7@gmail.com>
2022-10-15 14:02:44 +0100

    UI: adjust page header content to show auth user name

--- resources/views/livewire/pages/blocklist-filament.blade.php
+++ resources/views/livewire/pages/blocklist-filament.blade.php
@@ -1,5 +1,12 @@
 <div class="w-full h-full mx-auto bg-secondary-50 max-w-7xl">
-    <x-page-header>Blocklist</x-page-header>
+    <x-page-header>
+        <h1 class="text-sm font-bold uppercase text-secondary-500">
+            Blocklist
+        </h1>
+        <h1 class="font-bold">
+            Hello, {{ auth()->user()->first_name }}
+        </h1>
+    </x-page-header>
 
     <div class="px-3 py-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         {{ $this->table }}
