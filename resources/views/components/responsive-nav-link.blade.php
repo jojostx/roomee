f1b11bc040d1e89de450c2c16e2406059fb469da
Ari Jay <ikuskid7@gmail.com>
2022-10-17 05:50:03 +0100

    refactor: extract nav link classes to css component class

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -6,8 +6,8 @@
 
 @php
     $classes = ($active ?? false)
-        ? 'cursor-pointer flex items-center pl-3 pr-4 py-2 border-l-4 border-primary-400 text-base font-medium text-primary-700 bg-primary-50 focus:outline-none focus:text-primary-800 focus:bg-primary-100 focus:border-primary-700 transition duration-150 ease-in-out'
-        : 'cursor-pointer flex items-center pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-secondary-600 hover:text-secondary-800 hover:bg-secondary-50 hover:border-secondary-300 focus:outline-none focus:text-secondary-800 focus:bg-secondary-50 focus:border-secondary-300 transition duration-150 ease-in-out';
+        ? 'responsive-nav-link active'
+        : 'responsive-nav-link';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
