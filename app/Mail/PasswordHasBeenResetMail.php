94ab874c39e342e8785143854c0a29aa9b176972
Ari Jay <ikuskid7@gmail.com>
2022-10-20 08:59:53 +0100

    feat: create and register listener that sends a mail notification to the user upon PasswordReset event

---
+++ app/Mail/PasswordHasBeenResetMail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordHasBeenResetMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Model|Authenticatable $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Your Password Has Been Reset',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'mail.password-has-been-reset-mail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Events\RoommateRequestUpdated;
+use App\Listeners\SendPasswordHasBeenResetNotification;
 use App\Listeners\SendRoommateRequestUpdatedNotifications;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -24,6 +26,9 @@ class EventServiceProvider extends ServiceProvider
             SendRoommateRequestUpdatedNotifications::class,
         ],
         
+        PasswordReset::class => [
+            SendPasswordHasBeenResetNotification::class,
+        ]
     ];
 
     /**
